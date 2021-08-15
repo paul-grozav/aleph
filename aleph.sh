9871b4e31b541b72aea136508bda0630e539769c
Tancredi-Paul Grozav <paul@grozav.info>
2021-08-15 14:52:17 +0300

    x

--- aleph.sh
+++ aleph.sh
@@ -13,7 +13,11 @@ debian_base_image=debian:10.10 &&
 # docker:
 container_marker=/.dockerenv &&
 # podman (requires to be ran as root):
-#function docker(){ podman ${@} ; } && container_marker=/run/.containerenv &&
+if command -v podman &> /dev/null
+then
+  function docker(){ podman ${@} ; } && container_marker=/run/.containerenv
+fi &&
+
 
 
 # ============================================================================ #
